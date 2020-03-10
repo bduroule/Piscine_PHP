24c4f0763d5450fc30527590a90b160848b03e6e
Benjamin Duroule <beduroul@z3r2p3.le-101.fr>
2020-03-10 19:15:07 +0100

    delet

--- Rush00/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = '10.3.3.1';
-$user = 'root';
-$pass = 'root';
-$dbname = 'website';
-$conn = mysqli_connect($host, $user, $pass,$dbname);
-if(!$conn)
-	die('Could not connect: '.mysqli_connect_error());
-?>
